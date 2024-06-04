10700e64abb2450853466910e134f2ac8f695862
Prela 9 <amir.prelic@gmail.com>
2024-06-04 10:42:44 +0000

    Advanced js rest parameter

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ To use this repository:
 - Click the "Use this template" button in the upper right
 - Choose a name for your repository and create it
 - In your own repository, click the green "Gitpod" button
+
+# In this code:
+
+ - A regular function sum accepts two parameters and returns their sum.
+ - Another function sumIgnoreExtraArgs is defined with the same purpose, but it logs all passed arguments, demonstrating that extra 
+   arguments are ignored.
+ - Finally, a function sumWithRest is defined using the rest parameter syntax (...numbers). This function accepts any number of 
+   arguments and logs them as an array, then returns their sum using the reduce method.
--- rest-parameter-syntax.js
+++ rest-parameter-syntax.js
@@ -4,7 +4,25 @@
  */
 
 // Regular function call 
-
-// Extra arguments are ignored
-
-// Function using ...rest
+function sum(a, b) {
+    return a + b;
+  }
+  
+  console.log('Regular function call:', sum(5, 10)); // Output: 15
+  
+  // Extra arguments are ignored
+  function sumIgnoreExtraArgs(a, b) {
+    console.log('Arguments:', arguments);
+    return a + b;
+  }
+  
+  console.log('Extra arguments are ignored:', sumIgnoreExtraArgs(5, 10, 15)); // Output: Arguments: [5, 10, 15] 15
+  
+  // Function using ...rest
+  function sumWithRest(...numbers) {
+    console.log('Numbers:', numbers);
+    return numbers.reduce((acc, cur) => acc + cur, 0);
+  }
+  
+  console.log('Function using ...rest:', sumWithRest(5, 10, 15)); // Output: Numbers: [5, 10, 15] 30
+  
